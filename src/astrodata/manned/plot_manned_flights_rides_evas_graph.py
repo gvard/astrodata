a6934e2c10a45ecfd324bab4fd207c439aa9b1ae
Dmitry Nasonov <gvardopo4ta@gmail.com>
2023-05-29 17:20:49 +0300

    Add ArgumentParser, more flexible code for plotting graphs

--- src/astrodata/manned/plot_manned_flights_rides_evas_graph.py
+++ src/astrodata/manned/plot_manned_flights_rides_evas_graph.py
@@ -12,6 +12,7 @@ from datetime import datetime, timedelta
 import os
 import locale
 import urllib.request
+from argparse import ArgumentParser
 
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
@@ -37,12 +38,12 @@ def unite_legends(axes):
     """Hack to display legend over other plots
     https://github.com/matplotlib/matplotlib/issues/3706#issuecomment-164265176
     """
-    h, l = [], []
+    han, lab = [], []
     for ax in axes:
         tmp = ax.get_legend_handles_labels()
-        h.extend(tmp[0])
-        l.extend(tmp[1])
-    return h, l
+        han.extend(tmp[0])
+        lab.extend(tmp[1])
+    return han, lab
 
 def get_table(url):
     """Read ASCII data table from pre html element"""
@@ -62,6 +63,14 @@ DATA_BASE_URL = "https://planet4589.org/space/astro/lists/"
 MISSIONS_URL = DATA_BASE_URL + "missions.html"
 EVAS_URL = DATA_BASE_URL +  "evas.html"
 FLIGHTS_URL = "https://www.worldspaceflight.com/bios/chronology.php"
+
+parser = ArgumentParser(description='Graph plotter script with number of people in space')
+parser.add_argument('-r', '--rides', action='store_true', help='Plot rides')
+parser.add_argument('-e', '--evas', action='store_true', help='Plot EVAs')
+parser.add_argument('-a', '--astros', action='store_true', help='Plot astronauts numbers')
+parser.add_argument('-f', '--flights', action='store_true', help='Plot flights numbers')
+parser.add_argument('-c', '--counts', action='store_true', help='Plot EVAs counts')
+args = parser.parse_args()
 
 data = get_table(MISSIONS_URL)[1:]
 dates, flights_nums, rides_nums = [], [], []
@@ -124,18 +133,41 @@ today = datetime.now()
 MONTH, YEAR = today.strftime("%B"), today.year
 
 # Graph settings
-nums, ylim_top_margin, FILENAME = rides_nums, 50, "mannedflights-astronauts-rides-evas"
-# nums, ylim_top_margin, FILENAME = flights_nums, 15, "mannedflights"
-# nums, ylim_top_margin, FILENAME = flight_nums, 15, "mannedflights-astronauts"
-# nums, ylim_top_margin, FILENAME = evanums, 15, "mannedflights-evas"
-
-YLIM = nums[-1]+ylim_top_margin
 TM = timedelta(weeks=48)
 TM_SHIFT = timedelta(weeks=12)
 SH, SH2 = 13, 230
-accidents = [(1967, 1, 27, 'Пожар на «Аполлоне-1»', SH, TM),
-    (1967, 4, 23, 'Гибель В.М. Комарова, «Союз-1»', SH, -TM_SHIFT),
-    (1971, 6, 29, 'Катастрофа «Союз-11»', SH, TM),
+YLIM_TOP_MARGIN = 15
+TITLE = 'Присутствие человека в космосе'
+if args.rides:
+    HIGHEST_NUM = rides_nums[-1]
+    YLIM_TOP_MARGIN = 50
+    ax.plot(dates, rides_nums, '.b', label='Количество посещений', ms=6)
+elif not args.rides and args.evas:
+    HIGHEST_NUM = evanums[-1]
+    SH, SH2 = 9, 160
+elif not args.rides and not args.evas and args.astros:
+    HIGHEST_NUM = flight_nums[-1]
+    SH, SH2 = 6, 102
+elif not args.rides and not args.evas and not args.astros and args.flights:
+    HIGHEST_NUM = flights_nums[-1]
+    SH, SH2 = 2, 64
+    TITLE = 'Количество космических полетов'
+YLIM = HIGHEST_NUM + YLIM_TOP_MARGIN
+if args.evas:
+    ax.plot(evadates, evanums, '.', label='Количество ВКД', color="brown", ms=6)
+if args.astros:
+    ax.plot(flight_dates, flight_nums, '.k', label='Количество астронавтов', ms=6)
+if args.flights:
+    ax.plot(dates, flights_nums, '.m', label='Количество космических полетов', ms=6)
+
+
+accidents = [
+    (1961, 3, 23, 'Пожар, гибель В.В. Бондаренко', SH, -TM_SHIFT),
+    (1967, 1, 27, 'Пожар на «Аполлоне-1»', SH, TM),
+    (1967, 4, 23, 'Гибель Комарова, нераскрытие парашюта «Союза-1»', SH, -2.9*TM_SHIFT),
+    (1967, 11, 15, 'X-15, гибель М.Д. Адамса', SH, -TM_SHIFT*3.9),
+    (1971, 6, 29, 'Разгерметизация «Союза-11»', SH, TM),
+    (1980, 3, 18, 'Катастрофа на космодроме Плесецк', SH, TM),
     (1986, 1, 28, 'Катастрофа «Челленджера»', SH2, TM),
     (2003, 2, 1, 'Катастрофа «Колумбии»', SH, TM)]
 private_spaceflights = [(2001, 4, 28, 'Деннис Тито', SH2, TM),
@@ -144,30 +176,26 @@ private_spaceflights = [(2001, 4, 28, 'Деннис Тито', SH2, TM),
 
 for ac in accidents:
     dat = datetime(year=ac[0], month=ac[1], day=ac[2])
-    plt.plot((dat, dat), (0, nums[-1]+ylim_top_margin), '--r') # 2500
+    plt.plot((dat, dat), (0, HIGHEST_NUM+YLIM_TOP_MARGIN), '--r')
     ax.text(dat-ac[5], YLIM-ac[4], ac[3], rotation='vertical', va="top", fontsize=14)
 for ac in private_spaceflights:
     dat = datetime(year=ac[0], month=ac[1], day=ac[2])
-    plt.plot((dat, dat), (0, nums[-1]+ylim_top_margin), '--g') # 2500
+    plt.plot((dat, dat), (0, HIGHEST_NUM+YLIM_TOP_MARGIN), '--g')
     ax.text(dat-ac[5], YLIM-ac[4], ac[3], rotation='vertical', va="top", fontsize=14)
 
-plt.plot(dates, nums, '.b', label='Количество посещений', ms=6)
-plt.plot(evadates, evanums, '.', label='Количество ВКД', color="brown", ms=6)
-plt.plot(flight_dates, flight_nums, '.k', label='Количество астронавтов', ms=6)
-plt.plot(dates, flights_nums, '.m', label='Количество космических полетов', ms=6)
-plt.xlim(dates[0]-tdlt, dates[-1]+tdlt) # datetime(2060, 1, 1)
-plt.ylim(-3, nums[-1]+ylim_top_margin) # 2500
-plt.legend(fontsize=13)
+ax.set_xlim(dates[0]-tdlt, dates[-1]+tdlt) # datetime(2060, 1, 1)
+ax.set_ylim(-3, HIGHEST_NUM+YLIM_TOP_MARGIN) # 2500
+ax.legend(fontsize=13, loc='upper center')
 
-# plt.title(f'Количество космических полетов. Всего {orb_num} орбитальных и ' + \
-# plt.title(f'Число астронавтов. Всего {orb_num} орбитальных и ' + \
-# plt.title(f'Количество посещений. Всего {orb_num} орбитальных и ' + \
-plt.title(f'Присутствие человека в космосе. Всего {orb_num} орбитальных и ' + \
+plt.title(f'{TITLE}. Всего {orb_num} орбитальных и ' + \
     f'{suborb_num} суборбитальных полетов, {flight_nums[-1]} астронавтов, ' + \
     f'{evanums[-1]} ВКД и {rides_nums[-1]} посещений. {MONTH} {YEAR} года')
 plt.xlabel('Время, годы', fontsize=14)
 plt.ylabel('Количество', fontsize=14)
 plt.grid(linestyle='dotted')
+
+FILENAME = f"manned{'flights' if args.flights else ''}{'-astronauts' if args.astros else ''}" + \
+    f"{'-rides' if args.rides else ''}{'-evas' if args.evas else ''}"
 
 FILE_EXT = 'svg'
 plots_dir = os.path.join(os.pardir, os.pardir, os.pardir, 'plots', 'manned')
@@ -182,36 +210,36 @@ if FILE_EXT == 'svg':
 
 # SECOND PLOT
 
-fig, ax = plt.subplots(figsize=(16, 9))
+fig, bx = plt.subplots(figsize=(16, 9))
 fig.subplots_adjust(0.048, 0.06, 0.94, 0.97)
 years = mdates.YearLocator(5)
-ax.xaxis.set_major_locator(years)
-ax.xaxis.set_minor_locator(mdates.YearLocator())
-ax.yaxis.set_major_locator(MultipleLocator(25))
+bx.xaxis.set_major_locator(years)
+bx.xaxis.set_minor_locator(mdates.YearLocator())
+bx.yaxis.set_major_locator(MultipleLocator(25))
 
 tdlt = timedelta(days=300)
 plt.xlim(evadates[0]-tdlt, evadates[-1]+tdlt)
 plt.plot(evadates, evatds, 'ok', label='Суммарная длительность ВКД', ms=3)
 plt.grid(linestyle='dotted')
 
-plt.title(f'Полная длительность ВКД, {round(evas_total_time.total_seconds()/86400, 1)} суток в {len(evadates)} выходах. {MONTH} {YEAR} года')
-ax.set_xlabel('Время, годы', fontsize=14)
-ax.set_ylabel('Суммарная длительность ВКД, сутки', fontsize=14)
+plt.title(f'Полная длительность ВКД, {round(evas_total_time.total_seconds()/86400, 1)}' + \
+          f' суток в {len(evadates)} выходах. {MONTH} {YEAR} года')
+bx.set_xlabel('Время, годы', fontsize=14)
+bx.set_ylabel('Суммарная длительность ВКД, сутки', fontsize=14)
 plt.grid(linestyle='dotted')
 
-COUNTS = '-counts'
-if COUNTS:
-    ax2 = ax.twinx()
-    ax2.plot(evadates, evanums, '.', label='Количество ВКД', color="brown", ms=6)
-    ax.set_ylim(bottom=-0.7)
-    ax2.set_ylim(bottom=-1.5)
-    ax2.set_ylabel('Количество ВКД', fontsize=14)
-    ax2.set_facecolor("none")
+if args.counts:
+    bx2 = bx.twinx()
+    bx2.plot(evadates, evanums, '.', label='Количество ВКД', color="brown", ms=6)
+    bx.set_ylim(bottom=-0.7)
+    bx2.set_ylim(bottom=-1.5)
+    bx2.set_ylabel('Количество ВКД', fontsize=14)
+    bx2.set_facecolor("none")
 
-handles, labels = unite_legends([ax, ax2])
-ax.legend(handles, labels, loc='upper left', fontsize=13)
+handles, labels = unite_legends([bx, bx2])
+bx.legend(handles, labels, loc='upper left', fontsize=13)
 
-FILENAME = 'evas-total-time' + COUNTS
+FILENAME = f"evas-total-time{'-counts' if args.counts else ''}"
 FILE_EXT = 'svg'
 tmp_pth = os.path.join(plots_dir, FILENAME+'_.'+FILE_EXT)
 pth = os.path.join(plots_dir, FILENAME+'.'+FILE_EXT)
